e439cd9606baed9830d21c2fa39e755b4b7dc56e
Anatoly Bayusov <woland.rtf@gmail.com>
2020-07-09 22:50:31 +0600

    add solution for 4 module 2 task

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -10,9 +11,47 @@ server.on('request', (req, res) => {
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
-    case 'POST':
-
+    case 'POST': {
+      if (pathname.indexOf('/') !== -1) {
+        res.statusCode = 400;
+        res.end('Nested path not access');
+        return;
+      }
+      
+      fs.stat(filepath, (err, stats) => {
+        if (err) {
+          let body = '';
+          
+          req.on('data', (chunk) => {
+            body += chunk;
+          });
+          
+          req.on('end', () => {
+            if (body.length > 1000000) {
+              res.statusCode = 413;
+              res.end();
+            } else {
+              fs.writeFile(filepath, body, (err) => {
+                if (err) {
+                  throw err;
+                } else {
+                  res.statusCode = 201;
+                  res.end();
+                }
+              });
+            }
+          })
+        } else {
+          if (stats.isFile()) {
+            res.statusCode = 409;
+            res.end('File exist');
+          } else {
+            console.log('not used section');
+          }
+        }
+      });
       break;
+    }
 
     default:
       res.statusCode = 501;
