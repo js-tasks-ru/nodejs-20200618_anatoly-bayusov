043e678364e5bf423a42f608c2dabbe92c1287ce
Anatoly Bayusov <woland.rtf@gmail.com>
2020-07-09 15:43:45 +0600

    add solution for 4 module 1 task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,17 +1,42 @@
 const url = require('url');
 const http = require('http');
+const fs = require('fs');
 const path = require('path');
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on('request', async (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
-    case 'GET':
-
+    case 'GET': {
+      if (pathname.indexOf('/') !== -1) {
+        res.statusCode = 400;
+        res.end('Nested path not access');
+        return;
+      } else {
+        fs.stat(filepath, (err, stats) => {
+          if (typeof stats === 'undefined') {
+            res.statusCode = 404;
+            res.end('Not found');
+          } else {
+            if (stats.isFile()) {
+              fs.readFile(filepath, (err, data) => {
+                if (err) throw err;
+                res.statusCode = 200;
+                res.write(data);
+                res.end();
+              });
+            } else {
+              res.statusCode = 400;
+              res.end('Not file');
+            }
+          }
+        });
+      }
+    }
       break;
 
     default:
