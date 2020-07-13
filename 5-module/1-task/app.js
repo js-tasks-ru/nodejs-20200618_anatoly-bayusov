d2717cfbf3dea81c7c9413ad7f0f999350c0c439
Anatoly Bayusov <woland.rtf@gmail.com>
2020-07-13 23:08:19 +0600

    add solution for 5 module 1 task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -8,10 +8,48 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/subscribe', async (ctx, next) => {
+let clients = [];
+
+router.get("/subscribe", async (ctx, next) => {
+  
+  ctx.set("Cache-Control", "no-cache,must-revalidate");
+  const promise = new Promise((resolve, reject) => {
+    clients.push(resolve);
+    
+    ctx.res.on("close", () => {
+      clients.splice(clients.indexOf(resolve), 1);
+      const error = new Error("Connection closed");
+      error.code = "ECONNRESET";
+      reject(error);
+    });
+    
+  });
+  
+  let message;
+  
+  try {
+    message = await promise;
+  } catch (err) {
+    if (err.code === "ECONNRESET") return;
+    throw err;
+  }
+  ctx.body = message;
 });
 
-router.post('/publish', async (ctx, next) => {
+router.post("/publish", async (ctx, next) => {
+  const message = ctx.request.body.message;
+  
+  if (!message) {
+    ctx.throw(400);
+  }
+  
+  clients.forEach( (resolve) => {
+    resolve(String(message));
+  });
+  
+  clients = [];
+  
+  ctx.body = "ok";
 });
 
 app.use(router.routes());
