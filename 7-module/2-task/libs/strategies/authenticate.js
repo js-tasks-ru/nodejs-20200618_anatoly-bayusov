c93ede9e8b8eb08ce9d7eedeedba269127e6bff8
Anatoly Bayusov <woland.rtf@gmail.com>
2020-07-27 23:23:11 +0600

    add solution for 7 module 2 task

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,3 +1,27 @@
+const User = require('../../models/User');
+
 module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+  if (!email) {
+    done(null, false, `Не указан email`);
+  } else {
+    User.findOne({ email: email }, async function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      
+      if (!user) {
+        const newUser = new User({email, displayName});
+        try {
+          const newUserSaved = await newUser.save();
+          return done(null, newUserSaved);
+        } catch(error) {
+          return done(error, false);
+        }
+
+        
+      } else {
+        return done(null, user);
+      }
+    });
+  }
 };
